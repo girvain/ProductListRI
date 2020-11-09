b5919d796f082f3d2981780524c90397d23c6cd1
girvain <gavinross88@hotmail.co.uk>
2020-11-09 20:31:28 +0000

    Initial Commit

---
+++ ProductListRI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProductListRI
+//
+//  Created by Gavin Ross on 09/11/2020.
+//  Copyright © 2020 Gavin Ross. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ProductListRITests/ProductListRITests.swift
@@ -0,0 +1,34 @@
+//
+//  ProductListRITests.swift
+//  ProductListRITests
+//
+//  Created by Gavin Ross on 09/11/2020.
+//  Copyright © 2020 Gavin Ross. All rights reserved.
+//
+
+import XCTest
+@testable import ProductListRI
+
+class ProductListRITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
